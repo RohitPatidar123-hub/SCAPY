62992fb8bef9c77d4e82e06a27134f9b7cb1a7a1
RohitPatidar123-hub <genzcoader@gmail.com>
2024-08-18 11:26:01 +0530

    reading pcap file

---
+++ 3_reading_pcapfile.py
@@ -0,0 +1,36 @@
+from  scapy.all import *
+
+import pyx
+import reportlab
+from PIL import Image
+
+print("PyX version:", pyx.__version__)
+print("ReportLab version:", reportlab.Version)
+print("Pillow version:", Image.__version__)
+
+A=rdpcap("C:\\Users\\rohit\\Downloads\\speedtest.pcapng")
+print(A)
+b=PacketList(A)
+print(b)
+#a[423].pdfdump("output.pdf",layer_shift=1)
+#a[423].psdump("output.eps",layer_shift=1)
+print(len(A[423]))                          ##size of packet a[423]
+a=IP(dst="www.google.com/30")
+print(a.show())
+b=[p for p in a]
+print(b)                             #
+c=IP(ttl=[1,2,{4,5,6,7,7},(4,5)]) 
+print("We can fill field with multiple value :")   ##we can fill field with multiple value
+print(c.show())
+b=[p for p in c]
+print("We can also expand independetly also : ")                     ##we can also expand independently also for eack ttl
+print(b)
+e=TCP(dport=[4,453])
+b=[p for p in a/e]
+print("Total 8 packet :")
+print(b)
+a=PacketList(b)
+print(a)
+
+
+
